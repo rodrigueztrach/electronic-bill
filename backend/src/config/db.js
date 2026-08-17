1ba024ee88444753a23f52a2705bf8f99ef72f49
rodrigueztrach <rronny045@gmail.com>
2026-08-16 21:22:34 -0600

    feat: quiet large text of query

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
-    logging: process.env.NODE_ENV === 'development' ? console.log : false,
+    logging: false,
     define: {
       underscored: true,
       timestamps: true,
